fcf5e3cba5a0afa9e82eceb379e2f79189e5f69c
pache78 <jmpacheco78@gmail.com>
2017-02-15 11:44:59 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('require');
+var bodyParser =require('body-parser');
+var methodOverride = require('metho-override');
+
+var app = express();
+
+app.use(bodyParser.urlencoded{
+	extend:false
+})
+
+
